e82fdb9f16878e6eb2ae848e2c5ba0afdc266c64
AlberdaNazarius <103203230+AlberdaNazarius@users.noreply.github.com>
2023-10-27 16:15:10 +0300

    Added description slicing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {QuestionListComponent} from "./question-list/question-list.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "search", component: QuestionListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
