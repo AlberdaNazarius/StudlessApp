33e9abc208105eb4667d04e92208384ff61ab75a
AlberdaNazarius <103203230+AlberdaNazarius@users.noreply.github.com>
2023-11-01 17:21:48 +0200

    Added dynamic adaptation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    const header = document.getElementById("main-header");
+    if (header != null) {
+      document.body.style.paddingTop = `${header.getBoundingClientRect().height}px`
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: UIEvent) {
+    const header = document.getElementById("main-header");
+    if (header != null) {
+      document.body.style.paddingTop = `${header.getBoundingClientRect().height}px`
+    }
+  }
+}
