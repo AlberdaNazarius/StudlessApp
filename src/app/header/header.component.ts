991519673c2f8805d0648f66169606f4673d5b60
AlberdaNazarius <103203230+AlberdaNazarius@users.noreply.github.com>
2023-11-01 15:57:41 +0200

    Fixed header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
-
+export class HeaderComponent{
+  // TODO this values should be changed when person is logged or not
+  isLogged: boolean = true;
+  nickName: string = "UserNickname";
 }
