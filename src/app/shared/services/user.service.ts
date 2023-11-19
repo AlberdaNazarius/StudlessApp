05ca542d3ce4acf9a40b3144a92c5387335f4db4
AlberdaNazarius <103203230+AlberdaNazarius@users.noreply.github.com>
2023-11-19 17:55:18 +0200

    Added routing to all links

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -3,10 +3,21 @@ import { User } from '../models/user.model';
 
 @Injectable()
 export class UserService {
-  private user: User = new User(1, "assets/images/profile-logo.png", "assets/images/profile-banner.png","fda", "moderator", 331, 3234, 0, 1, 6258, 22435, new Date('2023-11-01'), 5)
-    
+  private users: User[] = [
+    new User(0, "assets/images/profile-logo.png", "assets/images/profile-banner.png","FirstUser", "moderator", 331, 3234, 0, 1, 6258, 22435, new Date('2023-11-01'), 5),
+    new User(1, "assets/images/profile-logo.png", "assets/images/profile-banner.png","SecondUser", "moderator", 331, 3234, 0, 1, 6258, 22435, new Date('2023-11-01'), 5),
+    new User(2, "assets/images/profile-logo.png", "assets/images/profile-banner.png","ThirdUser", "moderator", 331, 3234, 0, 1, 6258, 22435, new Date('2023-11-01'), 5),
+    new User(3, "assets/images/profile-logo.png", "assets/images/profile-banner.png","FourthUser", "moderator", 331, 3234, 0, 1, 6258, 22435, new Date('2023-11-01'), 5)
+  ]
 
-  public getUser(): User {
-    return this.user;
+  private authUser: User = this.users[0];
+
+
+  public getUser(id: number): User {
+    return this.users[id];
+  }
+
+  public getAuthenticatedUser(): User {
+    return this.authUser;
   }
 }
